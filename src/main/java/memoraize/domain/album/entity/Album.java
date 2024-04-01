f70948003444e62d8d8e7d3c3a12de74f85222f1
junhaa <2171326@hansung.ac.kr>
2024-04-02 02:21:11 +0900

    :sparkles: FEAT. Photo, Album 연관 관계 편의 메서드 추가

--- src/main/java/memoraize/domain/album/entity/Album.java
+++ src/main/java/memoraize/domain/album/entity/Album.java
@@ -7,9 +7,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import memoraize.domain.album.enums.AlbumAccess;
+import memoraize.domain.album.validation.annotation.ListNotBlank;
+import memoraize.domain.photo.entity.Photo;
 import memoraize.global.entity.BaseEntity;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -32,21 +35,41 @@ public class Album extends BaseEntity {
     @Column(name = "albumInfo", nullable = false)
     private String albumInfo;
 
-    @NotBlank
-    @Column(name = "photoURL", nullable = false)
     // 사진 url을 받기 위해 .. photo entity가 따로 있어야 하는데 아직 없어서 임시로 string으로..
     // 윤석오빠가 url, metadata를 photo 객체에 담아서 나한테 주면 내가 그걸 map api에 넣는다..
-    private List<String> photoURL;
-    //@OneToMany
-    //private List<Photo> photoURL
+
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photoImages = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     private AlbumAccess albumAccess;
 
-    @Column(name = "isDeleted")
-    @ColumnDefault("true")
+
+    @Column(name = "isDeleted", nullable = false)
     private Boolean isDeleted;
 
     //@ManyToOne
     //private User user;
+
+    @Override
+    public String toString() {
+        return "Album{" +
+            "albumId=" + albumId +
+            ", albumName='" + albumName + '\'' +
+            ", albumInfo='" + albumInfo + '\'' +
+            ", photoImages=" + photoImages +
+            ", albumAccess=" + albumAccess +
+            ", isDeleted=" + isDeleted +
+            '}';
+    }
+
+
+    // 연관관계 편의 메서드
+    public void addPhotoImages(List<Photo> photoImages){
+        for(Photo p : photoImages){
+            this.photoImages.add(p);
+            p.setAlbum(this);
+        }
+    }
+
 }
--- src/main/java/memoraize/domain/photo/entity/Photo.java
+++ src/main/java/memoraize/domain/photo/entity/Photo.java
@@ -6,14 +6,18 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import memoraize.domain.album.entity.Album;
 import memoraize.global.entity.BaseEntity;
 
 @Entity
@@ -38,4 +42,13 @@ public class Photo extends BaseEntity {
 
 	@OneToMany(mappedBy = "photo", cascade = CascadeType.ALL)
 	private List<PhotoHashTag> photoHashTagList = new ArrayList<>();
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "album_id", nullable = false)
+	private Album album;
+
+	// 연관관계 편의 메서드
+	public void setAlbum(Album album){
+		this.album = album;
+	}
 }
