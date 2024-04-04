e3b77eb35169ad41caaa433517e6a10829048778
junhaa <2171326@hansung.ac.kr>
2024-04-04 22:56:41 +0900

    :sparkles: FEAT. 메타데이터 위도 경도 추출 기능 추가

--- src/main/java/memoraize/domain/photo/service/PhotoCommandServiceImpl.java
+++ src/main/java/memoraize/domain/photo/service/PhotoCommandServiceImpl.java
@@ -1,5 +1,7 @@
 package memoraize.domain.photo.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -7,6 +9,11 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.lang.GeoLocation;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.GpsDirectory;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,7 +43,14 @@ public class PhotoCommandServiceImpl implements PhotoCommandService {
 
 		for(MultipartFile image : request){
 
+			// 이미지 파일 위치 정보 추출
+			GeoLocation geoLocation = extractLocation(image);
+			log.info("geoLocation = {}", geoLocation);
 			// Google Vision API 호출
+
+			// Google map => 위치 호출
+
+
 
 			String uuid = UUID.randomUUID().toString();
 			Uuid savedUuid = uuidRepository.save(Uuid.builder().uuid(uuid).build());
@@ -50,11 +64,41 @@ public class PhotoCommandServiceImpl implements PhotoCommandService {
 				//해시태그 리스트 추가
 				.build();
 
-			// Photo savedPhoto = photoRepository.save(photo);
-			// photoList.add(savedPhoto);
+			// setPlace, place save
+
 			photoList.add(photo);
 		}
 
 		return photoList;
+	}
+
+	private static GeoLocation extractLocation(MultipartFile file) {
+		try {
+			// MultipartFile을 File로 변환. 실제 사용 시 임시 파일을 생성하고 사용 후 삭제해야 할 수 있음.
+			File imageFile = convertToFile(file);
+
+			// 사진의 메타데이터를 읽음
+			Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
+
+			// 메타데이터에서 GPS 디렉토리를 가져옴
+			GpsDirectory gpsDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+			GeoLocation geoLocation = gpsDirectory == null ? null : gpsDirectory.getGeoLocation();
+
+			// 만약 파일이 존재하면 삭제
+			if(imageFile != null && imageFile.exists())
+				imageFile.delete();
+
+			return geoLocation;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static File convertToFile(MultipartFile multipartFile) throws IOException {
+		// 파일 변환 로직 구현 => 운영체제마다 tmp 폴더에 file 생성
+		File file = new File(System.getProperty("java.io.tmpdir") + "/" + multipartFile.getOriginalFilename());
+		multipartFile.transferTo(file);
+		return file;
 	}
 }
