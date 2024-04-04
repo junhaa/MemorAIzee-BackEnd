0a6c160a0367df83ff0108b81f5625648954bfda
junhaa <2171326@hansung.ac.kr>
2024-04-04 22:55:23 +0900

    :sparkles: FEAT. Place entity 추가

---
+++ src/main/java/memoraize/domain/review/entity/Place.java
@@ -0,0 +1,48 @@
+package memoraize.domain.review.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import memoraize.domain.photo.entity.Photo;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "place_id")
+	private Long id;
+
+	@NotNull
+	private String name;
+
+	@OneToMany(mappedBy = "place", cascade = CascadeType.ALL)
+	private List<Photo> photoList = new ArrayList<>();
+
+	@OneToMany(mappedBy = "place", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Review> reviewList = new ArrayList<>();
+
+	// 연관관계 편의 메서드
+	public void addReview(Review review){
+		this.reviewList.add(review);
+	}
+
+	public void addPhoto(Photo photo){
+		this.photoList.add(photo);
+	}
+}
