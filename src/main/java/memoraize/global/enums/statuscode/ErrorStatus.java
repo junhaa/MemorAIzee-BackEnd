401044929cdde62ccd36537a46bcc21127d6cebb
junhaa <2171326@hansung.ac.kr>
2024-04-05 15:57:13 +0900

    :sparkles: FEAT. Validation 유효성 추가

--- src/main/java/memoraize/global/enums/statuscode/ErrorStatus.java
+++ src/main/java/memoraize/global/enums/statuscode/ErrorStatus.java
@@ -2,7 +2,7 @@ package memoraize.global.enums.statuscode;
 
 import org.springframework.http.HttpStatus;
 
-public enum ErrorStatus implements BaseCode{
+public enum ErrorStatus implements BaseCode {
 	// common
 	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
 	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
@@ -10,9 +10,9 @@ public enum ErrorStatus implements BaseCode{
 	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
 	// Album
-	_PHOTO_IMAGE_NOT_EXIST(HttpStatus.BAD_REQUEST, "ALBUM4001", "업로드된 사진이 없습니다.");
-
-
+	_PHOTO_IMAGE_NOT_EXIST(HttpStatus.BAD_REQUEST, "ALBUM4001", "업로드된 사진이 없습니다."),
+	_PAGE_NOT_NEGATIVE(HttpStatus.BAD_REQUEST, "ALBUM4002", "page번호나 pageCount는 음수일 수 없습니다."),
+	_INVALID_SORT_STATUS(HttpStatus.BAD_REQUEST, "ALBUM4003", "정렬 기준이 올바르지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
