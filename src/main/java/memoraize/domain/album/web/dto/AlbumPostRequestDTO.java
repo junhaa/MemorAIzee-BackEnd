7e0475c3fc99310f273d0a86f3fc88308ecb314c
junhaa <2171326@hansung.ac.kr>
2024-04-05 15:54:55 +0900

    :sparkles: FEAT. Album Page 요청 DTO 추가

--- src/main/java/memoraize/domain/album/web/dto/AlbumPostRequestDTO.java
+++ src/main/java/memoraize/domain/album/web/dto/AlbumPostRequestDTO.java
@@ -4,23 +4,41 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import memoraize.domain.album.enums.AlbumAccess;
+import memoraize.domain.album.enums.SortStatus;
 import memoraize.domain.album.validation.annotation.ListNotBlank;
+import memoraize.domain.album.validation.annotation.NonNegative;
 
 public class AlbumPostRequestDTO {
 
-    @Getter
-    @Setter
-    public static class addAlbumPostDTO{
-        private String albumName;
-        private String albumInfo;
-        //private User user;
-        @ListNotBlank
-        private List<MultipartFile> images;
-        private AlbumAccess albumAccess;
-        private Boolean isDeleted;
-    }
+	@Getter
+	@Setter
+	public static class addAlbumPostDTO {
+		private String albumName;
+		private String albumInfo;
+		//private User user;
+		@ListNotBlank
+		private List<MultipartFile> images;
+		private AlbumAccess albumAccess;
+		private Boolean isDeleted;
+	}
 
+	@Getter
+	@Setter
+	public static class getAlbumPostPageDTO {
+
+		@NotNull(message = "정렬 기준은 필수입니다.")
+		private SortStatus sortStatus;
+
+		@NonNegative
+		@NotNull(message = "page값은 필수입니다.")
+		private Integer page;
+
+		@NonNegative
+		@NotNull(message = "albumCount값은 필수입니다.")
+		private Integer albumCount;
+	}
 }
