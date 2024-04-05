5c46f7ee8e82209fea5608106cf7adc52ba65d00
junhaa <2171326@hansung.ac.kr>
2024-04-05 15:54:55 +0900

    :sparkles: FEAT. Album Page 요청 DTO 추가

--- src/main/java/memoraize/domain/album/web/dto/AlbumPostResponseDTO.java
+++ src/main/java/memoraize/domain/album/web/dto/AlbumPostResponseDTO.java
@@ -1,6 +1,7 @@
 package memoraize.domain.album.web.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +13,35 @@ public class AlbumPostResponseDTO {
 	@Getter
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class addAlbumPostResultDTO{
+	public static class AddAlbumPostResultDTO {
 		private Long albumId;
 		private LocalDateTime createdAt;
 	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class AlbumPostPreviewResultPageDTO {
+		private List<AlbumPostPreviewResultDTO> albums;
+		private Integer listSize;
+		private Integer totalPage;
+		private Long totalElements;
+		private Boolean isFirst;
+		private Boolean isLast;
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class AlbumPostPreviewResultDTO {
+		private Long albumId;
+		private String mainImageUrl;
+		private String albumName;
+		private LocalDateTime createdAt;
+
+		// TODO HashTag 추가 및 앨범 URL 추가
+	}
+
 }
