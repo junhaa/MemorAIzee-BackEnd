20d4062912cdb4391a564eb311d7314e88879cde
junhaa <2171326@hansung.ac.kr>
2024-04-30 03:11:59 +0900

    :sparkles: FEAT. GoogleOAuth2UserInfo.java Google OAuth2 사용자 정보 매핑 클래스 작성

---
+++ src/main/java/memoraize/global/security/oauth/GoogleOAuth2UserInfo.java
@@ -0,0 +1,28 @@
+package memoraize.global.security.oauth;
+
+import java.util.Map;
+
+/**
+ * Google Social Login시 반환되는 정보
+ */
+public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
+
+	public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+		super(attributes);
+	}
+
+	@Override
+	public String getId() {
+		return (String) attributes.get("sub");
+	}
+
+	@Override
+	public String getNickname() {
+		return (String) attributes.get("name");
+	}
+
+	@Override
+	public String getImageUrl() {
+		return (String) attributes.get("picture");
+	}
+}
