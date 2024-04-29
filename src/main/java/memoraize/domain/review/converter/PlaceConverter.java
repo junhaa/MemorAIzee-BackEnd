0d09dc582b93ea04001b994827647936077823eb
junhaa <2171326@hansung.ac.kr>
2024-04-29 18:16:17 +0900

    :art: CHORE. Photo <-> Place OneToOne으로 변경

--- src/main/java/memoraize/domain/review/converter/PlaceConverter.java
+++ src/main/java/memoraize/domain/review/converter/PlaceConverter.java
@@ -8,7 +8,6 @@ public class PlaceConverter {
 	public static Place toPlace(String placeName) {
 		return Place.builder()
 			.placeName(placeName)
-			.photoList(new ArrayList<>())
 			.reviewList(new ArrayList<>())
 			.build();
 	}
