638f1a11d4c9dcf9321a0d9b0f5a6b5409d4383c
junhaa <2171326@hansung.ac.kr>
2024-04-30 03:01:03 +0900

    :sparkles: FEAT. JwtLoginFailureHandler.java JWT Filter 인증 실패 Handler 추가

---
+++ src/main/java/memoraize/global/security/oauth/JwtLoginFailureHandler.java
@@ -0,0 +1,28 @@
+package memoraize.global.security.oauth;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+/**
+ * JWT 로그인 필터 Failure Handler
+ */
+public class JwtLoginFailureHandler implements AuthenticationFailureHandler {
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws
+		IOException, ServletException {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		response.getWriter().write("JWT 로그인에 실패, 서버 로그를 확인해주세요.");
+		log.info("Jwt Login fail :: error = {}", exception.getMessage());
+	}
+}
+
