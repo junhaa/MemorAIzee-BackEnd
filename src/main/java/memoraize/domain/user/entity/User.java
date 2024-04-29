6f1fdd41dbcec39de0296084fef320b502b0f5d0
junhaa <2171326@hansung.ac.kr>
2024-04-30 02:46:53 +0900

    :sparkles: FEAT. User.java Entity 추가

---
+++ src/main/java/memoraize/domain/user/entity/User.java
@@ -0,0 +1,88 @@
+package memoraize.domain.user.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import memoraize.domain.album.entity.Album;
+import memoraize.domain.user.entity.mapping.AlbumLiked;
+import memoraize.domain.user.enums.LoginType;
+import memoraize.global.entity.BaseEntity;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "login_id", nullable = false)
+	private String loginId;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "user_name", nullable = false)
+	private String userName;
+
+	@Column(name = "phone_number", nullable = true)
+	private String phoneNumber;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "login_type", nullable = false)
+	private LoginType loginType;
+
+	@Column(name = "refresh_token")
+	private String refreshToken;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Album> albumList = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<AlbumLiked> albumLikedList = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Authority> authorityList = new ArrayList<>();
+
+
+	// 연관 관계 편의 메서드
+
+	public void addAlbum(Album album){
+		albumList.add(album);
+		album.setUser(this);
+	}
+
+	public void addAlbumLiked(AlbumLiked albumLiked){
+		albumLikedList.add(albumLiked);
+		albumLiked.setUser(this);
+	}
+
+	public void addAuthority(Authority authority){
+		authorityList.add(authority);
+		authority.setUser(this);
+	}
+
+	public void updateRefreshToken(String refreshToken){
+		this.refreshToken = refreshToken;
+	}
+
+
+}
