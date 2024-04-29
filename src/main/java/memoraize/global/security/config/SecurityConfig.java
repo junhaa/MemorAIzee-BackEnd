f1a08c6232cca77a22973138c99bcf06a08c17c7
junhaa <2171326@hansung.ac.kr>
2024-04-30 03:15:35 +0900

    :sparkles: FEAT. SecurityConfig.java Spring Security Configuration 작성

---
+++ src/main/java/memoraize/global/security/config/SecurityConfig.java
@@ -0,0 +1,147 @@
+package memoraize.global.security.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+import memoraize.domain.user.repository.UserRepository;
+import memoraize.domain.user.service.UserQueryService;
+import memoraize.global.security.LoginService;
+import memoraize.global.security.jwt.JwtService;
+import memoraize.global.security.jwt.filter.CustomUsernamePwdAuthenticationFilter;
+import memoraize.global.security.jwt.filter.JWTTokenGeneratorFilter;
+import memoraize.global.security.jwt.filter.JWTTokenValidatorFilter;
+import memoraize.global.security.jwt.filter.JwtAuthenticationFilter;
+import memoraize.global.security.jwt.filter.RequestValidationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import memoraize.global.security.oauth.CustomOAuth2UserService;
+import memoraize.global.security.oauth.JwtLoginFailureHandler;
+import memoraize.global.security.oauth.JwtLoginSuccessHandler;
+import memoraize.global.security.oauth.OAuth2LoginFailureHandler;
+import memoraize.global.security.oauth.OAuth2LoginSuccessHandler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+/**
+ * 인증은 CustomJsonUsernamePasswordAuthenticationFilter에서 authenticate()로 인증된 사용자로 처리
+ * JwtAuthenticationProcessingFilter는 AccessToken, RefreshToken 재발급
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final LoginService loginService;
+	private final JwtService jwtService;
+	private final UserRepository userRepository;
+	private final ObjectMapper objectMapper;
+	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+	private final JwtLoginSuccessHandler jwtLoginSuccessHandler;
+	private final JwtLoginFailureHandler jwtLoginFailureHandler;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final UserQueryService userQueryService;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable) // csrf 보안 사용 X => Rest API
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Token 기반 인증 => session 사용 X
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/login", "/api/user/signup", "/", "/favicon.ico").permitAll() // 허용된 주소
+				.anyRequest().authenticated()
+			)
+			// CORS
+			.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("*"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setExposedHeaders(Arrays.asList("Authorization"));
+				config.setMaxAge(3600L);
+				return config;
+			}));
+			// OAUTH2 config
+			http.oauth2Login(httpSecurityOAuth2LoginConfigurer -> {
+				httpSecurityOAuth2LoginConfigurer
+					.successHandler(oAuth2LoginSuccessHandler)
+					.failureHandler(oAuth2LoginFailureHandler)
+					.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService));
+			});
+
+		http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+		http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomUsernamePwdAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder());
+		provider.setUserDetailsService(loginService);
+		return new ProviderManager(provider);
+	}
+
+
+	@Bean
+	public CustomUsernamePwdAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+		CustomUsernamePwdAuthenticationFilter customJsonUsernamePasswordLoginFilter
+			= new CustomUsernamePwdAuthenticationFilter(objectMapper);
+		customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+		customJsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(jwtLoginSuccessHandler);
+		customJsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(jwtLoginFailureHandler);
+		return customJsonUsernamePasswordLoginFilter;
+	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationProcessingFilter() {
+		JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService,
+			userRepository, userQueryService);
+		return jwtAuthenticationFilter;
+	}
+}
