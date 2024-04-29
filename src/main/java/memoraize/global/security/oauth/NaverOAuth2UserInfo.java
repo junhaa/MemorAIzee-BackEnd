1c6c49b5f5a471efc6f52f6b67cbf007cdc64b33
junhaa <2171326@hansung.ac.kr>
2024-04-30 03:12:28 +0900

    :sparkles: FEAT. NaverOAuth2UserInfo.java Naver OAuth2 사용자 정보 매핑 클래스 작성

---
+++ src/main/java/memoraize/global/security/oauth/NaverOAuth2UserInfo.java
@@ -0,0 +1,45 @@
+package memoraize.global.security.oauth;
+
+import java.util.Map;
+/**
+ * Naver Social Login시 반환되는 Principal
+ */
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+	public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+		super(attributes);
+	}
+
+	@Override
+	public String getId() {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+		if (response == null) {
+			return null;
+		}
+		return (String) response.get("id");
+	}
+
+	@Override
+	public String getNickname() {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+		if (response == null) {
+			return null;
+		}
+
+		return (String) response.get("nickname");
+	}
+
+	@Override
+	public String getImageUrl() {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+		if (response == null) {
+			return null;
+		}
+
+		return (String) response.get("profile_image");
+	}
+}
+
