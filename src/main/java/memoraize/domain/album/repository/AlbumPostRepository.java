0bf6c65e9617362f88f5225e9881ecdca8ef8c2c
= <=>
2024-05-10 16:19:59 +0900

    :sparkles: FEAT. AlbumPostRepository 앨범 제목으로 앨범 검색 Jpa 메서드 추가

--- src/main/java/memoraize/domain/album/repository/AlbumPostRepository.java
+++ src/main/java/memoraize/domain/album/repository/AlbumPostRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import memoraize.domain.album.entity.Album;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AlbumPostRepository extends JpaRepository<Album, Long> {
 	Page<Album> findAll(Pageable pageable);
 
 	Page<Album> findByUserId(Long userId, Pageable pageable);
 
 	Long countByUserId(Long userId);
+
+	Optional<List<Album>> findByAlbumName(String albumName);
 }
