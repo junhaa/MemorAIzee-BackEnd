89ee4d02ebe813c1f47efe2777e025ab752b38dc
= <=>
2024-05-11 13:21:42 +0900

    :bug: FIX. 충돌 해결

--- src/main/java/memoraize/domain/review/repository/PlaceRepository.java
+++ src/main/java/memoraize/domain/review/repository/PlaceRepository.java
@@ -1,5 +1,6 @@
 package memoraize.domain.review.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import memoraize.domain.review.entity.Place;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 	Optional<Place> findByPlaceName(String placeName);
+	Optional<List<Place>> findAllByPlaceName(String placeNeme);
 }
