e2e3ad095f7da201983e1604140941a567fd2499
= <=>
2024-05-15 17:48:51 +0900

    :art: CHORE. 통합검색 메서드 findBy{찾으려는 내용}Containing으로 변경 및 Optional -> List로 반환형 변경

--- src/main/java/memoraize/domain/user/repository/UserRepository.java
+++ src/main/java/memoraize/domain/user/repository/UserRepository.java
@@ -1,45 +1,44 @@
 package memoraize.domain.user.repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import memoraize.domain.user.entity.User;
+import memoraize.domain.user.enums.LoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import memoraize.domain.user.entity.User;
-import memoraize.domain.user.enums.LoginType;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
 
-	Optional<User> findById(Long userId);
+    Optional<User> findById(Long userId);
 
-	Optional<User> findByRefreshToken(String refreshToken);
+    Optional<User> findByRefreshToken(String refreshToken);
 
-	Optional<User> findByLoginTypeAndLoginId(LoginType loginType, String LoginId);
+    Optional<User> findByLoginTypeAndLoginId(LoginType loginType, String LoginId);
 
-	boolean existsByLoginIdAndLoginType(String loginId, LoginType loginType);
+    boolean existsByLoginIdAndLoginType(String loginId, LoginType loginType);
 
-	@Query("SELECT u\n"
-		+ "FROM User u\n"
-		+ "WHERE u.id IN (\n"
-		+ "    SELECT f.following.id\n"
-		+ "    FROM Follow f\n"
-		+ "    WHERE f.follower.id = :userId\n"
-		+ ")")
-	List<User> findUsersFollowedBy(@Param("userId") Long userId);
+    @Query("SELECT u\n"
+            + "FROM User u\n"
+            + "WHERE u.id IN (\n"
+            + "    SELECT f.following.id\n"
+            + "    FROM Follow f\n"
+            + "    WHERE f.follower.id = :userId\n"
+            + ")")
+    List<User> findUsersFollowedBy(@Param("userId") Long userId);
 
-	@Query("SELECT u\n"
-		+ "FROM User u\n"
-		+ "WHERE u.id IN (\n"
-		+ "    SELECT f.follower.id\n"
-		+ "    FROM Follow f\n"
-		+ "    WHERE f.following.id = :userId\n"
-		+ ")")
-	List<User> findUsersFollowingBy(@Param("userId") Long userId);
+    @Query("SELECT u\n"
+            + "FROM User u\n"
+            + "WHERE u.id IN (\n"
+            + "    SELECT f.follower.id\n"
+            + "    FROM Follow f\n"
+            + "    WHERE f.following.id = :userId\n"
+            + ")")
+    List<User> findUsersFollowingBy(@Param("userId") Long userId);
 
-	Optional<List<User>> findByUserName(String userName);
+    List<User> findByUserNameContaining(String userName);
 }
