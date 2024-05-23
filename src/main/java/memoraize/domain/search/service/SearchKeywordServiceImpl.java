782bf2d4762288790a4830bcc4e1eb0a601fdcef
junhaa <2171326@hansung.ac.kr>
2024-05-23 17:38:03 +0900

    :sparkles: FEAT. 유저 검색 정보에 유저 프로필 사진 추가

--- src/main/java/memoraize/domain/search/service/SearchKeywordServiceImpl.java
+++ src/main/java/memoraize/domain/search/service/SearchKeywordServiceImpl.java
@@ -1,5 +1,11 @@
 package memoraize.domain.search.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import memoraize.domain.album.entity.Album;
 import memoraize.domain.album.repository.AlbumPostRepository;
@@ -10,73 +16,68 @@ import memoraize.domain.review.repository.ReviewRepository;
 import memoraize.domain.search.web.dto.SearchKeywordResponseDTO;
 import memoraize.domain.user.entity.User;
 import memoraize.domain.user.repository.UserRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SearchKeywordServiceImpl implements SearchKeywordService {
 
-    private final AlbumPostRepository albumPostRepository;
-    private final UserRepository userRepository;
-    private final PlaceRepository placeRepository;
-    private final ReviewRepository reviewRepository;
+	private final AlbumPostRepository albumPostRepository;
+	private final UserRepository userRepository;
+	private final PlaceRepository placeRepository;
+	private final ReviewRepository reviewRepository;
 
-    // 검색 메서드
-    @Override
-    public SearchKeywordResponseDTO.SearchResultDTO searchKeyword(String keyword) {
+	// 검색 메서드
+	@Override
+	public SearchKeywordResponseDTO.SearchResultDTO searchKeyword(String keyword) {
 
-        List<Album> resultAlbumList = albumPostRepository.findByAlbumNameContaining(keyword);
-        List<User> resultUserList = userRepository.findByUserNameContaining(keyword);
-        List<Place> resultPlaceList = placeRepository.findAllByPlaceNameContaining(keyword);
-        List<Review> resultReviewList = reviewRepository.findByContextContaining(keyword);
+		List<Album> resultAlbumList = albumPostRepository.findByAlbumNameContaining(keyword);
+		List<User> resultUserList = userRepository.findByUserNameContaining(keyword);
+		List<Place> resultPlaceList = placeRepository.findAllByPlaceNameContaining(keyword);
+		List<Review> resultReviewList = reviewRepository.findByContextContaining(keyword);
 
+		List<SearchKeywordResponseDTO.AlbumInfo> albumInfo = new ArrayList<>();
+		for (Album album : resultAlbumList) {
+			albumInfo.add(SearchKeywordResponseDTO.AlbumInfo.builder()
+				.id(album.getId())
+				.albumName(album.getAlbumName())
+				.userName(album.getUser().getUserName())
+				.mainImg(album.getPhotoImages().get(0).getImageUrl())
+				.build());
+		}
 
-        List<SearchKeywordResponseDTO.AlbumInfo> albumInfo = new ArrayList<>();
-        for (Album album : resultAlbumList) {
-            albumInfo.add(SearchKeywordResponseDTO.AlbumInfo.builder()
-                    .id(album.getId())
-                    .albumName(album.getAlbumName())
-                    .userName(album.getUser().getUserName())
-                    .mainImg(album.getPhotoImages().get(0).getImageUrl())
-                    .build());
-        }
+		List<SearchKeywordResponseDTO.UserInfo> userInfo = new ArrayList<>();
+		for (User user : resultUserList) {
+			userInfo.add(SearchKeywordResponseDTO.UserInfo.builder()
+				.id(user.getId())
+				.userId(user.getLoginId())
+				.userName(user.getUserName())
+				.profileImg(user.getImageUrl())
+				.build());
+		}
 
-        List<SearchKeywordResponseDTO.UserInfo> userInfo = new ArrayList<>();
-        for (User user : resultUserList) {
-            userInfo.add(SearchKeywordResponseDTO.UserInfo.builder()
-                    .id(user.getId())
-                    .userId(user.getLoginId())
-                    .userName(user.getUserName())
-                    .build());
-        }
+		List<SearchKeywordResponseDTO.PlaceInfo> placeInfo = new ArrayList<>();
+		for (Place place : resultPlaceList) {
+			placeInfo.add(SearchKeywordResponseDTO.PlaceInfo.builder()
+				.id(place.getId())
+				.placeName(place.getPlaceName())
+				.build());
+		}
 
-        List<SearchKeywordResponseDTO.PlaceInfo> placeInfo = new ArrayList<>();
-        for (Place place : resultPlaceList) {
-            placeInfo.add(SearchKeywordResponseDTO.PlaceInfo.builder()
-                    .id(place.getId())
-                    .placeName(place.getPlaceName())
-                    .build());
-        }
+		List<SearchKeywordResponseDTO.ReviewInfo> reviewInfo = new ArrayList<>();
+		for (Review review : resultReviewList) {
+			reviewInfo.add(SearchKeywordResponseDTO.ReviewInfo.builder()
+				.id(review.getId())
+				.rate(review.getStar())
+				.context(review.getContext())
+				.build());
+		}
 
-        List<SearchKeywordResponseDTO.ReviewInfo> reviewInfo = new ArrayList<>();
-        for (Review review : resultReviewList) {
-            reviewInfo.add(SearchKeywordResponseDTO.ReviewInfo.builder()
-                    .id(review.getId())
-                    .rate(review.getStar())
-                    .context(review.getContext())
-                    .build());
-        }
-
-        return SearchKeywordResponseDTO.SearchResultDTO.builder()
-                .albumList(albumInfo)
-                .userList(userInfo)
-                .placeList(placeInfo)
-                .reviewList(reviewInfo)
-                .build();
-    }
+		return SearchKeywordResponseDTO.SearchResultDTO.builder()
+			.albumList(albumInfo)
+			.userList(userInfo)
+			.placeList(placeInfo)
+			.reviewList(reviewInfo)
+			.build();
+	}
 }
